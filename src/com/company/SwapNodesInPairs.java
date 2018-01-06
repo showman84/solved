4191df0d56aa0fd0c98fc3db401894f70bdb344d
showman84 <shuaib.rasool@gmail.com>
2018-01-05 16:59:14 -0800

    two more

---
+++ src/com/company/SwapNodesInPairs.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class SwapNodesInPairs {
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current = current.next.next;
+        }
+        return dummy.next;
+    }
+    public static void main(String[] args) {
+
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
+        System.out.println(new SwapNodesInPairs().swapPairs(node));
+    }
+}
