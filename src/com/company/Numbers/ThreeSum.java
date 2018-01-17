4791fea97f5c3b7f39347675bf67d35e3c68226b
showman84 <shuaib.rasool@gmail.com>
2018-01-16 17:20:38 -0800

    two more

--- src/com/company/Numbers/ThreeSum.java
+++ src/com/company/Numbers/ThreeSum.java
@@ -18,23 +18,38 @@ public class ThreeSum {
 
     private void fillTwoSum(int [] nums, int targetIndex,  List<List<Integer>> result) {
         Hashtable<Integer, Integer> map = new Hashtable();
-        int target = nums[targetIndex];
+        int target = -1 * nums[targetIndex];
         for (int i = 0; i < nums.length;) {
             if (i == targetIndex) {
                 i++;
                 continue;
             }
-            if (map.containsKey(-(target - nums[i]))) {
+            if (map.containsKey((target - nums[i]))) {
                 List<Integer> temp = new ArrayList<>();
                 temp.add(nums[i]);
-                temp.add(nums[map.get(-(target - nums[i]))]);
+                temp.add(nums[map.get((target - nums[i]))]);
                 temp.add(nums[targetIndex]);
-                result.add(temp);
-
+                if(!tripletExits(result, temp)) {
+                    result.add(temp);
+                }
             }
             map.put(nums[i], i);
             i++;
         }
+    }
+
+    public boolean tripletExits(List<List<Integer>> result, List<Integer> entry) {
+
+        for (List<Integer> local: result) {
+            int count = 0;
+            for(int i: local) {
+                if(entry.contains(i)) {
+                   count++;
+                }
+                if (count == 3) return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
