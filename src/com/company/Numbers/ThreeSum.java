665bf90d85d95d8898c1282ad2be14a0c5539c4d
showman84 <shuaib.rasool@gmail.com>
2018-01-16 16:51:48 -0800

    two more

---
+++ src/com/company/Numbers/ThreeSum.java
@@ -0,0 +1,44 @@
+package com.company.Numbers;
+
+import com.company.Arrays.RotateArray;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+public class ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int i = 0; i < nums.length; i++) {
+            fillTwoSum(nums, i, result);
+        }
+        return result;
+    }
+
+    private void fillTwoSum(int [] nums, int targetIndex,  List<List<Integer>> result) {
+        Hashtable<Integer, Integer> map = new Hashtable();
+        int target = nums[targetIndex];
+        for (int i = 0; i < nums.length;) {
+            if (i == targetIndex) {
+                i++;
+                continue;
+            }
+            if (map.containsKey(-(target - nums[i]))) {
+                List<Integer> temp = new ArrayList<>();
+                temp.add(nums[i]);
+                temp.add(nums[map.get(-(target - nums[i]))]);
+                temp.add(nums[targetIndex]);
+                result.add(temp);
+
+            }
+            map.put(nums[i], i);
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int numbers [] = new int[]{-1, 0, 1, 2, -1, -4};
+        System.out.println(new ThreeSum().threeSum(numbers));
+    }
+}
